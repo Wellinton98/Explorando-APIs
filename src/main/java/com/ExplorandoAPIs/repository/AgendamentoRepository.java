c6294c42ffaff1c578aeae359c955614352a63b6
Maira Geraldo <maira_geraldo@estudante.sesisenai.org.br>
2026-04-02 23:58:02 -0300

    refactor: ajusta repository para suportar validação de conflito de agendamento

--- src/main/java/com/ExplorandoAPIs/repository/AgendamentoRepository.java
+++ src/main/java/com/ExplorandoAPIs/repository/AgendamentoRepository.java
@@ -2,13 +2,12 @@ package com.ExplorandoAPIs.repository;
 
 import com.ExplorandoAPIs.model.Agendamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Optional;
 
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
 
-    // Verifica se já existe um agendamento na mesma data e horário
     Optional<Agendamento> findByDataAndHorario(LocalDate data, LocalTime horario);
-
 }
